2278924e8636aa2e5108b2438b2d6b9758bbf34f
spark <ulfyyang@gmail.com>
2019-02-14 15:40:53 +0800

    feat: 添加BaseActivity依赖

--- android_libs/init_template/ui/BaseActivity.java
+++ android_libs/init_template/ui/BaseActivity.java
@@ -1,5 +1,8 @@
 package {{ package_name }}.ui.base;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import com.ulfy.android.extra.base.UlfyBaseActivity;
 
 public abstract class BaseActivity extends UlfyBaseActivity {
