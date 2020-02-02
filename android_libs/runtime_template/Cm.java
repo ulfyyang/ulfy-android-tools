8687a693d9aee8d4b0e911be3284440f73e488d5
ulfyyang <ulfy920814>
2020-02-02 17:43:09 +0800

    去除CM中的默认执行方法

--- android_libs/runtime_template/Cm.java
+++ android_libs/runtime_template/Cm.java
@@ -8,21 +8,6 @@ import {{ package_name }}.ui.cell.{{ model_name }}Cell;
 
 public class {{ model_name }}CM extends BaseCM {
 
-    public LoadDataUiTask.OnExecute loadDataOnExe() {
-        return new LoadDataUiTask.OnExecute() {
-            @Override public void onExecute(LoadDataUiTask task) {
-                try {
-                    task.notifyStart("正在加载...");
-
-                    task.notifySuccess("加载完成");
-                } catch (Exception e) {
-                    LogUtils.log("加载失败", e);
-                    task.notifyFail(e);
-                }
-            }
-        };
-    }
-
     @Override public Class<? extends IView> getViewClass() {
         return {{ model_name }}Cell.class;
     }
